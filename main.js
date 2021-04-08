9e0ad7da95748a045da54e4a7f45208ebdbe4220
AldairMC <aldairmc2023@gmail.com>
2021-04-08 16:46:57 -0500

    :sparkles: Function handleImages ready

---
+++ main.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", async function(event) {
+//     let consulta = await fetch('https://api.unsplash.com/photos/?client_id=_n7_LVkcrHUocVS7Xo9yph5ppJ2t_h55nrAWz-WXf9s')
+//         .then(response => response.json())
+//         .then(data => data);
+    
+//     handleImages(consulta)
+// });
+
+const handleImages = (data) => {
+    return data.map(i => {
+        var capa = document.getElementById("container_images");
+        var img = document.createElement("img");
+        img.src = `${i.urls.regular}`
+        capa.appendChild(img);
+    })
+}
